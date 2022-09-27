3a06321061d1a4df26fa2e766d9347333f1460d0
chendj89 <chendj89@hotmail.com>
2022-09-27 20:55:35 +0800

    664635

--- cli/rollup.config.ts
+++ cli/rollup.config.ts
@@ -5,13 +5,24 @@ import json from "@rollup/plugin-json";
 import tsc from "rollup-plugin-typescript2";
 // 导出配置
 
-export default defineConfig({
-  input: "./src/index.ts",
-  output: {
-    file: "dist/index.js",
-    format: "es",
-    banner: "#! /usr/bin/env node",
+export default defineConfig([
+  {
+    input: "./src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "es",
+      banner: "#! /usr/bin/env node",
+    },
+    external: ["http", "path", "mime", "fs", "commander"],
+    plugins: [json(), tsc()],
   },
-  external: ["http", "path", "mime", "fs", "commander"],
-  plugins: [json(), tsc()],
-});
+  {
+    input: "./src/gitdownload.ts",
+    output: {
+      file: "dist/gitdownload.js",
+      format: "es",
+    },
+    external: ["http", "path", "mime", "fs", "commander"],
+    plugins: [json(), tsc()],
+  },
+]);
